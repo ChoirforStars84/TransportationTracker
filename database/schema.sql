51b5ddf4f53dbdbe83cacf900b2f359b04299311
Julie Cohen <julie.r.cohen@gmail.com>
2018-12-11 17:13:33 -0500

    baby's first commit

--- database/schema.sql
+++ database/schema.sql
@@ -16,4 +16,47 @@ CREATE TABLE app_user (
   phone_number varchar(10) NULL
 );
 
+DROP TABLE IF EXISTS bus_stops;
+
+CREATE TABLE app_user (
+  internalid varchar(8) PRIMARY KEY,
+  name varchar(128) NOT NULL,
+  externalid varchar(8) NOT NULL,
+  direction varchar(16) NOT NULL,
+  lat varchar(16) NOT NULL,
+  long varchar(16) NOT NULL,
+  time_point varchar(8) NOT NULL,
+  newzone varchar(8) NOT NULL,
+  no_rts_served varchar(8) NOT NULL,
+  routes varchar(128) NOT NULL, 
+  mode varchar(16) NOT NULL,
+  public_shelter varchar(32) NOT NULL,
+  public_stop varchar(32) NOT NULL
+  
+);
+
+DROP TABLE IF EXISTS bus_lines;
+
+CREATE TABLE bus_lines (
+	num varchar(8) PRIMARY KEY,
+	name varchar(64)
+
+);
+
+
+
+DROP TABLE IF EXISTS stops_lines;
+
+CREATE TABLE stops_lines (
+	bus_line varchar(16),
+	stop_id varchar(8),
+	
+	constraint pk_stops_lines primary key (bus_line, stop_id),
+    constraint fk_stops_lines_bus_line foreign key (bus_line) references bus_lines (number),
+    constraint fk_stops_lines_stop_id foreign key (stop_id) references stops (internalid)
+
+);
+
+
+
 COMMIT;
