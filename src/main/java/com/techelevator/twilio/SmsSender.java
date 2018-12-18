6348f44f3d9b5dfe76a810ae147e23aa0e1a5fcc
Jonathan Wooding <jonathanwooding@Techelevators-MacBook-Air-79.local>
2018-12-18 13:53:43 -0500

    smssender and homepage edit

--- src/main/java/com/techelevator/twilio/SmsSender.java
+++ src/main/java/com/techelevator/twilio/SmsSender.java
@@ -6,10 +6,8 @@ import com.twilio.type.PhoneNumber;
 
 public class SmsSender {
     // Find your Account Sid and Auth Token at twilio.com/console
-    public static final String ACCOUNT_SID =
-            "AC849c5d9cb707893f69f97b4fde619bdd";
-    public static final String AUTH_TOKEN =
-            "03568f45394683648dcfbbb03782410d";
+    public static final String ACCOUNT_SID = "AC849c5d9cb707893f69f97b4fde619bdd";
+    public static final String AUTH_TOKEN = "03568f45394683648dcfbbb03782410d";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
